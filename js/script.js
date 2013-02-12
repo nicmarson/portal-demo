b7ad37918737c5a03cb04708fde4d43f997ca4f0
Nicholas Marson <nicmarson@gmail.com>
2013-02-12 15:32:37 -0800

    finished home and added contact us

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
 $(function() {
   
-  // Position buttons in equal height containers
-  $(".btn-container-bottom .btn-positioned").addClass("btn-positioned-on");
-
   // Add visual toggle icons
-  $('ol.breadcrumb a').after(' <span class="divider" role="separator">/</span>');
   $('[data-toggle="dropdown"]').append(' <b class="caret"></b>');
-  $('[data-toggle="collapse"]').prepend('<b class="caret"></b> ');
+  $('[data-toggle="collapse"]').append(' <b class="caret"></b>');
   $('nav#nav div.container').prepend('<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">'+
     '<span class="icon-bar"></span>'+
     '<span class="icon-bar"></span>'+
     '<span class="icon-bar"></span>'+
   '</a>');
 
-  // If browser doesn't support autofocus
-  // Then focus on input with autofocus attr
-  if (!Modernizr.input.autofocus) {
-    $('input[autofocus]').focus();
-  }
+  // Anystretch Plugin for Responsive Images
+  // This takes a background image and resizes it based on window size
+  // * USE background-image: NOT background: *
+  $('div.image-responsive').each(function() {
+    var $resDiv = $('div.image-responsive');
+    var resUrl = $resDiv.css("background-image").replace(/"/g,"").replace(/url\(|\)$/ig, "");
+    if ($(this).hasClass('focus-center')) {
+      $resDiv.anystretch(resUrl);
+    }
+    if ($(this).hasClass('focus-left')) {
+      $resDiv.anystretch(resUrl, {positionX: 'left'});
+    }
+    if ($(this).hasClass('focus-right')) {
+      $resDiv.anystretch(resUrl, {positionX: 'right'});
+    }
+  });
+  // This takes an inline image and resizes it based on window size
+  $('img.image-responsive').each(function() {
+    var $resImg = $('img.image-responsive');
+    var resSrc = $resImg.hide().attr('src');
+    if ($(this).hasClass('focus-center')) {
+      $resImg.parent().anystretch(resSrc);
+    }
+    if ($(this).hasClass('focus-left')) {
+      $resImg.parent().anystretch(resSrc, {positionX: 'left'});
+    }
+    if ($(this).hasClass('focus-right')) {
+      $resImg.parent().anystretch(resSrc, {positionX: 'right'});
+    }
+  });
 
 });
